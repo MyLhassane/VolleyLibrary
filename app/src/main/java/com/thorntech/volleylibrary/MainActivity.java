f1719f6e54385302b4003fd78c41280873948759
mastinoz <persiste1@gmail.com>
2016-12-16 12:20:52 +0000

    Advanced Example with JSON Array

--- app/src/main/java/com/thorntech/volleylibrary/MainActivity.java
+++ app/src/main/java/com/thorntech/volleylibrary/MainActivity.java
@@ -1,25 +1,25 @@
 package com.thorntech.volleylibrary;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.JsonArrayRequest;
+        import com.android.volley.toolbox.Volley;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     // Will show the string "data" that holds the results
     TextView results;
     // URL of object to be parsed
-    String JsonURL = "https://raw.githubusercontent.com/ianbar20/JSON-Volley-Tutorial/master/Example-JSON-Files/Example-Object.JSON";
+    String JsonURL = "https://raw.githubusercontent.com/ianbar20/JSON-Volley-Tutorial/master/Example-JSON-Files/Example-Array.JSON";
     // This string will hold the results
     String data = "";
     // Defining the Volley request queue that handles the URL request concurrently
@@ -35,28 +35,38 @@ public class MainActivity extends AppCompatActivity {
         // Casts results into the TextView found within the main layout XML with id jsonData
         results = (TextView) findViewById(R.id.jsonData);
 
-        // Creating the JsonObjectRequest class called obreq, passing required parameters:
-        //GET is used to fetch data from the server, JsonURL is the URL to be fetched from.
-        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, JsonURL,
-                // The third parameter Listener overrides the method onResponse() and passes
-                //JSONObject as a parameter
-                new Response.Listener<JSONObject>() {
+        // Creating the JsonArrayRequest class called arrayreq, passing the required parameters
+        //JsonURL is the URL to be fetched from
+        JsonArrayRequest arrayreq = new JsonArrayRequest(JsonURL,
+                // The second parameter Listener overrides the method onResponse() and passes
+                //JSONArray as a parameter
+                new Response.Listener<JSONArray>() {
 
                     // Takes the response from the JSON request
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONArray response) {
                         try {
-                            JSONObject obj = response.getJSONObject("colorObject");
-                            // Retrieves the string labeled "colorName" and "description" from
-                            //the response JSON Object
-                            //and converts them into javascript objects
-                            String color = obj.getString("colorName");
-                            String desc = obj.getString("description");
+                            // Retrieves first JSON object in outer array
+                            JSONObject colorObj = response.getJSONObject(0);
+                            // Retrieves "colorArray" from the JSON object
+                            JSONArray colorArry = colorObj.getJSONArray("colorArray");
+                            // Iterates through the JSON Array getting objects and adding them
+                            //to the list view until there are no more objects in colorArray
+                            for (int i = 0; i < colorArry.length(); i++) {
+                                //gets each JSON object within the JSON array
+                                JSONObject jsonObject = colorArry.getJSONObject(i);
 
-                            // Adds strings from object to the "data" string
-                            data += "Color Name: " + color +
-                                    "nDescription : " + desc;
+                                // Retrieves the string labeled "colorName" and "hexValue",
+                                // and converts them into javascript objects
+                                String color = jsonObject.getString("colorName");
+                                String hex = jsonObject.getString("hexValue");
 
+                                // Adds strings from the current object to the data string
+                                //spacing is included at the end to separate the results from
+                                //one another
+                                data += "Color Number " + (i + 1) + "nColor Name: " + color +
+                                        "nHex Value : " + hex + "nnn";
+                            }
                             // Adds the data string to the TextView "results"
                             results.setText(data);
                         }
@@ -77,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
-        // Adds the JSON object request "obreq" to the request queue
-        requestQueue.add(obreq);
+        // Adds the JSON array request "arrayreq" to the request queue
+        requestQueue.add(arrayreq);
     }
 }
